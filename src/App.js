dd8ae335af06002a20fe5a0782330e327b696bc9
Michael Baska <mikebaska1@gmail.com>
2020-05-06 21:34:28 -0500

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import AboutMe from "./pages/aboutMe";
 import Contact from "./pages/contact";
 import Portfolio from "./pages/portfolio";
+import Nav from "./components/Nav";
+import AntiNav from "./components/AntiNav";
 import {
   BrowserRouter,
   Route,
@@ -12,11 +14,13 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
         <Route exact path={["/", "/aboutme"]} component={AboutMe} />
         <Route path={"/contact"} component={Contact} />
         <Route path={"/portfolio"} component={Portfolio} />
       </Switch>
+      <AntiNav />
     </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Nav from "./components/Nav";
-import AntiNav from "./components/AntiNav";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Nav />
-    <App />
-    <AntiNav />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
