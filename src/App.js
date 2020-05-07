f060a33f6466d807f1d18ca9ef6bd6cd54041e59
Michael Baska <mikebaska1@gmail.com>
2020-05-07 12:57:16 -0500

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
+      <AboutMe />
       <Nav />
       <Switch>
         <Route exact path={["/", "/aboutme"]} component={AboutMe} />
